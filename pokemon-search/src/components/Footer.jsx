e3b86f0420803514a2af6d3cb526378416eee958
JoshuaYoshi1922 <joshua.yoshi1922@gmail.com>
2025-11-13 16:02:52 -0600

    some css

--- pokemon-search/src/components/Footer.jsx
+++ pokemon-search/src/components/Footer.jsx
@@ -1,16 +1,30 @@
 import PokemonQuestion from "./PokemonQuestion";
-
+import { FaGithub, FaLinkedin } from "react-icons/fa"
 function Footer() {
   return (
     <>
-    <div>
-      <footer className="footer">
-        <p>&copy;2025 ZBLcustoms</p>
-                <div className="question">
-          <PokemonQuestion />
-        </div>
-        <p>Kansas City, MO</p>
-      </footer>
+      <div>
+        <footer className="footer">
+          <p>&copy;2025 ZBLcustoms</p>
+          <a
+            href="https://github.com/JoshuaYoshi1922/unit-2-final-project-Joshua-L"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaGithub style={{ marginRight: 6 }} /> GitHub Repo
+          </a>
+          <div className="question">
+            <PokemonQuestion />
+          </div>
+          <a
+            href="https://linkedin.com/in/joshua-l-649530379"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaLinkedin style={{ marginRight: 6 }} /> LinkedIn
+          </a>
+          <p>Kansas City, MO</p>
+        </footer>
       </div>
     </>
   );
